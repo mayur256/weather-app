39c26142bf0ab58a636e1fea2bdc43ce8b6e3ec7
Mayur Upadhayay <mayur123a@gmail.com>
2022-06-25 15:05:54 +0530

    feat: Defined template components and added static assets

---
+++ src/components/templates/Main.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// Top level imports
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+
+const iconName: Ref<string> = ref('113');
+const iconDirPath = "../../assets/icons/day/"
+</script>
+
+<template>
+    <div class="container">
+        <h3 class="brand">the weather</h3>
+
+        <div>
+            <h1 class="temp">16&#176;</h1>
+
+            <div class="city-time">
+                <h1 class="city-name">London</h1>
+                <small>
+                    <span class="time">06:09</span>
+                    -
+                    <span class="date">
+                        Monday Sep 19
+                    </span>
+                </small>
+            </div>
+
+            <div class="weather">
+                <img src="../../assets/icons/day/113.png" class="icon" alt="icon" width="50" height="50" />
+                <span class="condition">Cloudy</span>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/templates/Panel.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="panel">
+        <form id="locationinput">
+            <input type="text" name="" class="search" placeholder="Search location" />
+
+            <button type="submit" class="submit">
+                <span class="material-icons">search</span>
+            </button>
+        </form>
+
+        <ul class="cities">
+            <li class="city">New York</li>
+            <li class="city">New Delhi</li>
+            <li class="city">Paris</li>
+            <li class="city">Tokyo</li>
+        </ul>
+
+        <ul class="details">
+            <h4 class="">Weather details</h4>
+            <li class="">
+                <span>Cloudy</span>
+                <span class="cloud">89%</span>
+            </li>
+            <li class="">
+                <span>Humidity</span>
+                <span class="humidity">64%</span>
+            </li>
+            <li class="">
+                <span>Wind</span>
+                <span class="wind">8km/h</span>
+            </li>
+
+        </ul>
+    </div>
+</template>
